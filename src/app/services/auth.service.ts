010c7804560a098121bf89285338c80c38a1b3ed
twfraley <twfraley@gmail.com>
2019-03-15 22:49:17 -0400

    Added Login/Authorization service

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -26,7 +26,7 @@ export class AuthService {
     return this._http.post(`${Api_Url}/token`, str).subscribe( (token: Token) => {
       localStorage.setItem('id_token', token.access_token);
       this.isLoggedIn.next(true);
-      this._router.navigate(['/notes']);
+      this._router.navigate(['/home']);
     });
   }
 
