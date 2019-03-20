348c1243701c5ab1fcd8ab1d5cb5ba6547743730
jonadcock <jonmichaeladcock@gmail.com>
2019-03-20 13:33:59 -0400

    joncontact

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { ReactiveFormsModule } from "@angular/forms";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { HttpClientModule } from "@angular/common/http";
 
 import {
@@ -53,6 +53,7 @@ import { LogoutComponent } from './components/user/logout/logout.component';
     BrowserModule,
     BrowserAnimationsModule,
     AppRoutingModule,
+    FormsModule,
     ReactiveFormsModule,
     HttpClientModule,
     MatToolbarModule,
