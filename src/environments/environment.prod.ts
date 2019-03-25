a4b6e056a9594ddd639db35f7643ddb3e11fb735
jonadcock <jonmichaeladcock@gmail.com>
2019-03-25 10:48:00 -0400

    cleanup app2

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,15 @@
 export const environment = {
   production: true
 };
+export let APIURL = '';
+
+switch (window.location.hostname) {
+    // this is the deployed angular application
+    case 'https://winterclient.herokuapp.com':
+        // this is the full url of your deployed API
+        APIURL = 'https://westerosfantasyleague.azurewebsites.com';
+        break;
+    default:
+        // this is the local host name of your API
+        APIURL = 'http://localhost:4200';
+}
